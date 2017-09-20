d8e09bd3ea76d28f65801ba1efed422ef62c3529
nicholas antonov <nicholasantonov@gmail.com>
2017-09-20 13:26:57 -0400

    Add all future date tests

--- tests/futuredates.py
+++ tests/futuredates.py
@@ -7,5 +7,21 @@ class ErrorOnFutureDates(unittest.TestCase):
         people, families, errors = parse('simplegood.ged')
         self.assertEqual(len(errors), 0)
 
+    def test_fail_on_future_date(self):
+        people, families, errors = parse('futurenick.ged')
+        self.assertEqual(len(errors), 1)
+
+    def test_fail_finds_id(self):
+        people, families, errors = parse('futurenick.ged')
+        self.assertEqual(errors[0].offenders[0], '@I1@')
+
+    def test_correct_number_of_offenders(self):
+        people, families, errors = parse('futurenick.ged')
+        self.assertEqual(len(errors[0].offenders), 1)
+
+    def test_correct_severity(self):
+        people, families, errors = parse('futurenick.ged')
+        self.assertEqual(errors[0].severity, 1)
+
 if __name__ == '__main__':
     unittest.main()
