d9b2ae30cca296b1001b4dfda359f52c252b9fec
nicholas antonov <nicholasantonov@gmail.com>
2017-10-10 13:02:22 -0400

    Fix multiple names test

--- tests/multiplefirst.py
+++ tests/multiplefirst.py
@@ -2,11 +2,6 @@ import unittest
 from parser import parse
 
 class ErrorOnMultipleFirstNames(unittest.TestCase):
-
-    def test_no_fail_multiplefirst(self):
-        people, families, errors = parse('multiplefirst.ged')
-        self.assertEqual(len(errors), 0)
-
     def test_fail_multiplefirst(self):
         people, families, errors = parse('multiplefirst.ged')
         self.assertEqual(len(errors), 1)
