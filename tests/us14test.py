08d31fad8c296cc051db6dd94c83221462e38a95
Matthew LaForgia <mlaforgia2@gmail.com>
2017-11-11 17:48:42 -0500

    fixed incorrectly written test

--- tests/us14test.py
+++ tests/us14test.py
@@ -9,7 +9,7 @@ class kids5onsameday(unittest.TestCase):
 
     def test_fail_greater5kids(self):
         people, families, errors, lists = parse('6kidsonsameday.ged')
-        self.assertEqual(len(errors), 0)
+        self.assertEqual(len(errors), 6)
 
 
 if __name__ == '__main__':
