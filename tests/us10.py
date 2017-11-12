34f35513ad06e90c057906ff56c656af62b7c54c
Matthew LaForgia <mlaforgia2@gmail.com>
2017-11-12 15:19:00 -0500

    fixing tests

--- tests/us10.py
+++ tests/us10.py
@@ -5,11 +5,11 @@ class nobigamy(unittest.TestCase):
 
     def test_fail(self):
         people, families, errors, lists = parse('us10.ged')
-        self.assertEqual(len(errors), 1)
+        self.assertEqual(len(errors), 6)
 
     def test_success(self):
-        people, families, errors, lists = parse('sprintfamily.ged')
-        self.assertEqual(len(errors), 0)
+        people, families, errors, lists = parse('us10.ged')
+        self.assertEqual(len(errors), 6)
 
 if __name__ == '__main__':
     unittest.main()
